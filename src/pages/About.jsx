abf7d5287ea38bb02f8ef4c13f1fa0b4d4c60d94
brenolimeira <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>
2026-03-12 20:09:14 -0300

    ajustes no about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,13 @@ const { Paragraph, Title } = Typography;
 
 export default function About() {
     return (
-        <Flex justify="center" vertical style={{ width: "100%" }}>
+        <Flex justify="center" gap={24} vertical style={{ width: "100%" }}>
             <Title style={{ color: "#fff", marginBottom: "24px" }}>Sobre mim</Title>
+             <Image
+                        src={image}
+                        alt="perfil"
+                        style={{ borderRadius: "50%", width: 250, height: 250,  objectFit: "cover" }}
+                    />
             <Flex justify="center" style={{ width: "100%" }}>
                 <Flex
                     wrap="wrap"
@@ -18,28 +23,23 @@ export default function About() {
                         gap: "40px"  
                     }}
                 >
-                    <Image
-                        src={image}
-                        width={200}
-                        alt="perfil"
-                        style={{ borderRadius: "50%" }}
-                    />
+                   
 
                     <Flex vertical style={{ maxWidth: "700px" }}>
-                        <Paragraph style={{ color: "#fff" }}>
+                        <Paragraph style={{ color: "#fff", fontSize: 16 }}>
                             Sou Breno Silvério, desenvolvedor fullstack com experiência no
                             desenvolvimento de aplicações web utilizando React, JavaScript,
                             Python e Django. Tenho interesse em criar soluções eficientes,
                             escaláveis e bem estruturadas.
                         </Paragraph>
 
-                        <Paragraph style={{ color: "#fff" }}>
+                        <Paragraph style={{ color: "#fff", fontSize: 16 }}>
                             Ao longo da minha trajetória, desenvolvi projetos utilizando
                             arquitetura baseada em API REST, autenticação de usuários e banco
                             de dados como PostgreSQL.
                         </Paragraph>
 
-                        <Paragraph style={{ color: "#fff" }}>
+                        <Paragraph style={{ color: "#fff", fontSize: 16 }}>
                             Atualmente, busco oportunidades para continuar evoluindo como
                             desenvolvedor e contribuir na criação de aplicações modernas,
                             performáticas e que gerem valor para usuários e empresas.
